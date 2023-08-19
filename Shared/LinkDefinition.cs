2b7d69b96b7c744a5c9968036daa8e09ce431442
Tan Hoang <hoangtan1193@gmail.com>
2023-08-19 14:41:35 +0700

    direct link from index page

---
+++ Shared/LinkDefinition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorApp.Shared
+{
+    public class LinkDefinition
+    {
+        public string Slug { get; set; }
+        public string DefaultUrl { get; set; }
+        public List<string> RegionSpecificUrls { get; set; }
+    }
+
+}
---
+++ Shared/LinkService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorApp.Shared
+{
+    public class LinkService
+    {
+        public List<LinkDefinition> GetLinkDefinitions()
+        {
+            // Define your link definitions here
+            var linkDefinitions = new List<LinkDefinition>
+        {
+            new LinkDefinition
+            {
+                Slug = "tui-nam",
+                DefaultUrl = "https://shope.ee/2VOqbwvDIv",
+                //RegionSpecificUrls = new List<string>
+                //{
+                //    "https://www.amazon.co.uk/product-1",
+                //    "https://www.amazon.de/product-1"
+                //}
+            }
+        };
+
+            return linkDefinitions;
+        }
+    }
+}

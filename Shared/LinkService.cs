5a381794281f2371573a917c1158c4f671669475
Tan Hoang <hoangtan1193@gmail.com>
2023-10-19 23:05:34 +0700

    format

--- Shared/LinkService.cs
+++ Shared/LinkService.cs
@@ -8,31 +8,20 @@ namespace BlazorApp.Shared
         {
             // Define your link definitions here
             var linkDefinitions = new List<LinkDefinition>
-        {
-            new LinkDefinition
             {
-                Slug = "handbag",
-                DefaultUrl = "https://shope.ee/2VOqbwvDIv",
-                PageTitle = "Teelab Crossbody Messenger Bag",
-                //RegionSpecificUrls = new List<string>
-                //{
-                //    "https://www.amazon.co.uk/product-1",
-                //    "https://www.amazon.de/product-1"
-                //}
-            },
-             new LinkDefinition
-            {
-                Slug = "keyboard1",
-                DefaultUrl = "https://shope.ee/5KjJHeb0Ms",
-                PageTitle = "Bàn phím cơ mạch xuôi JAMESDONKEY A3 ",
-                //RegionSpecificUrls = new List<string>
-                //{
-                //    "https://www.amazon.co.uk/product-1",
-                //    "https://www.amazon.de/product-1"
-                //}
-            }
-        };
-
+                new LinkDefinition
+                {
+                    Slug = "handbag",
+                    DefaultUrl = "https://shope.ee/2VOqbwvDIv",
+                    PageTitle = "Teelab Crossbody Messenger Bag",
+                },
+                 new LinkDefinition
+                {
+                    Slug = "keyboard1",
+                    DefaultUrl = "https://shope.ee/5KjJHeb0Ms",
+                    PageTitle = "Bàn phím cơ mạch xuôi JAMESDONKEY A3 ",
+                }
+            };
             return linkDefinitions;
         }
     }
